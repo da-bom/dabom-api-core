7278bb91a3aae23d24252c89089f4fcfc5171418
dnwldla <leeemjw@naver.com>
2026-02-16 16:49:33 +0900

    DABOM-184 feat: 가족 구성원의 사용량을 조회하는 로직 추가

--- src/main/java/com/project/domain/usagerecord/service/UsageRecordService.java
+++ src/main/java/com/project/domain/usagerecord/service/UsageRecordService.java
@@ -1,5 +1,7 @@
 package com.project.domain.usagerecord.service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -7,8 +9,11 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.project.domain.customer.repository.CustomerQuotaQueryRepository;
 import com.project.domain.family.infra.cache.FamilyCacheRepository;
 import com.project.domain.family.repository.FamilyMemberRepository;
+import com.project.domain.usagerecord.dto.response.CustomerUsageResponse;
+import com.project.domain.usagerecord.dto.response.RealtimeFamilyUsageResponse;
 import com.project.domain.usagerecord.dto.response.RealtimeTotalUsageResponse;
 import com.project.domain.usagerecord.infra.sse.UsageSseEmitterRegistry;
 import com.project.global.event.dto.usage.UsageRealtimePayload;
@@ -26,6 +31,7 @@ public class UsageRecordService {
     private final UsageSseEmitterRegistry registry;
     private final FamilyCacheRepository familyCacheRepository;
     private final FamilyMemberRepository familyMemberRepository;
+    private final CustomerQuotaQueryRepository customerQuotaQueryRepository;
 
     private final ConcurrentHashMap<Long, Long> lastSeen = new ConcurrentHashMap<>();
 
@@ -51,7 +57,37 @@ public class UsageRecordService {
     }
 
     public void pushMemberUsageBytes(UsageRealtimePayload payload) {
+        Long familyId = payload.familyId();
+        Long customerId = payload.customerId();
 
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        int month = now.getMonthValue();
+
+        // 전체 가족 구성원의 monthlyLimitBytes를 데이터베이스에서 조회한다
+        List<CustomerUsageResponse> customerUsageResponses =
+                customerQuotaQueryRepository.findCustomerUsage(familyId, customerId);
+
+        // 이벤트 대상이 아닌 가족 구성원들의 monthlyUsedBytes를 캐시에서 조회한다
+        for (CustomerUsageResponse customerUsageResponse : customerUsageResponses) {
+            long componentId = customerUsageResponse.getCustomerId();
+
+            if (componentId != customerId) {
+                Optional<Long> usageOpt =
+                        familyCacheRepository.findCustomerMonthlyUsageBytes(familyId, componentId);
+
+                Long usageByte = usageOpt.orElse(0L);
+                customerUsageResponse.setMonthlyUsedBytes(usageByte);
+
+            } else if (componentId == customerId) {
+                customerUsageResponse.setMonthlyUsedBytes(payload.monthlyUsedBytes());
+            }
+        }
+
+        // dto를 조합한다
+        RealtimeFamilyUsageResponse response =
+                new RealtimeFamilyUsageResponse(familyId, year, month, customerUsageResponses);
+        registry.send(familyId, "usage-updated-by-member", response);
     }
 
     /** 스케줄링을 통한 push 테스트 메소드 */
@@ -77,7 +113,7 @@ public class UsageRecordService {
                 UsageRealtimePayload payload =
                         new UsageRealtimePayload(
                                 familyId,
-                                null,
+                                1L,
                                 totalUsedBytes,
                                 totalLimitBytes,
                                 remainingBytes,
@@ -87,6 +123,7 @@ public class UsageRecordService {
                                 null);
 
                 pushTotalUsageBytes(payload);
+                pushMemberUsageBytes(payload);
             }
         }
     }
