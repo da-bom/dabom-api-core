dda6451c7a06e15952ea4f0bf28a6f9645791159
paul0755 <100present@naver.com>
2026-02-17 02:39:40 +0900

    DABOM-189 fix : 동일한 이름으로 인한 오류 해결

--- src/main/java/com/project/domain/customer/service/SignInService.java
+++ src/main/java/com/project/domain/customer/service/SignInService.java
@@ -1,8 +1,8 @@
 package com.project.domain.customer.service;
 
-import com.project.domain.customer.dto.request.SignInRequest;
+import com.project.domain.customer.dto.request.CustomerSignInRequest;
 import com.project.domain.customer.dto.response.SignInResponse;
 
 public interface SignInService {
-    SignInResponse signIn(SignInRequest requestDto);
+    SignInResponse signIn(CustomerSignInRequest requestDto);
 }
