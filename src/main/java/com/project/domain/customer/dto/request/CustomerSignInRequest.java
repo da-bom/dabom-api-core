14a8cb199430f2ce9236bdacd1637ae51e26a0d1
paul0755 <100present@naver.com>
2026-02-17 23:22:57 +0900

    DABOM-189 feat : 유저 로그인 검증 추가

--- src/main/java/com/project/domain/customer/dto/request/CustomerSignInRequest.java
+++ src/main/java/com/project/domain/customer/dto/request/CustomerSignInRequest.java
@@ -1,3 +1,6 @@
 package com.project.domain.customer.dto.request;
 
-public record CustomerSignInRequest(String phoneNumber, String password) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CustomerSignInRequest(@NotBlank(message = "전화번호 입력은 필수입니다") String phoneNumber,@Size(min=8, message = "최소 8글자 이상 입력해야합니다.") String password) {}
