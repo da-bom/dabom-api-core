d7acea461b34a916a7dd0b0b1032479e198ad10b
paul0755 <100present@naver.com>
2026-02-26 17:49:43 +0900

    DABOM-244 remove : example 코드 삭제

--- src/main/java/com/project/global/config/RedisConfig.java
+++ src/main/java/com/project/global/config/RedisConfig.java
@@ -11,30 +11,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.project.domain.example.infra.cache.dto.ExampleCacheDto;
 import com.project.domain.family.infra.cache.dto.FamilyCacheDto;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public RedisTemplate<String, ExampleCacheDto> exampleCacheRedisTemplate(
-            RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, ExampleCacheDto> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        StringRedisSerializer keySerializer = new StringRedisSerializer();
-        GenericJackson2JsonRedisSerializer valueSerializer =
-                new GenericJackson2JsonRedisSerializer();
-
-        template.setKeySerializer(keySerializer);
-        template.setHashKeySerializer(keySerializer);
-        template.setValueSerializer(valueSerializer);
-        template.setHashValueSerializer(valueSerializer);
-
-        template.afterPropertiesSet();
-        return template;
-    }
 
     @Bean
     public RedisTemplate<String, FamilyCacheDto> familyCacheRedisTemplate(
