3afae6b11cd2bd55d9a1e2097de57dd2f2e8100d
paul0755 <100present@naver.com>
2026-03-01 16:39:10 +0900

    DABOM-244 remove : port 패키지 삭제

--- src/main/java/com/project/domain/family/service/port/FamilyProducer.java
+++
@@ -1,7 +0,0 @@
-package com.project.domain.family.service.port;
-
-import com.project.domain.family.entity.Family;
-
-public interface FamilyProducer {
-    void produceFamilyCreated(Family family);
-}
