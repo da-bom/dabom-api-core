03cf70ead9347c0d38bd9f2c47183c5583b5f8e6
yechan <100present@naver.com>
2026-03-03 22:22:23 +0900

    DABOM-318 style : 코드 스타일 체크

--- src/test/java/com/project/domain/family/controller/FamilyControllerTest.java
+++ src/test/java/com/project/domain/family/controller/FamilyControllerTest.java
@@ -15,11 +15,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.domain.customer.enums.RoleType;
