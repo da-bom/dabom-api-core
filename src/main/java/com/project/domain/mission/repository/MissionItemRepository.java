cd6e82685111deec248eeb8ec187b5cf4e2ee69c
paul0755 <100present@naver.com>
2026-03-09 02:45:31 +0900

    DABOM-274 feat : 미션 엔티티 비관적 락 조회 메서드 추가

--- src/main/java/com/project/domain/mission/repository/MissionItemRepository.java
+++ src/main/java/com/project/domain/mission/repository/MissionItemRepository.java
@@ -3,8 +3,11 @@ package com.project.domain.mission.repository;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.persistence.LockModeType;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import com.project.domain.mission.entity.MissionItem;
@@ -13,6 +16,10 @@ import com.project.domain.mission.enums.MissionStatus;
 /** MissionItem DB 접근 전용 Repository다. */
 public interface MissionItemRepository extends JpaRepository<MissionItem, Long> {
     Optional<MissionItem> findByIdAndFamilyId(Long missionId, Long familyId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select mi from MissionItem mi where mi.id = :missionId and mi.familyId = :familyId")
+    Optional<MissionItem> findByIdAndFamilyIdForUpdate(Long missionId, Long familyId);
 
     List<MissionItem> findByFamilyIdOrderByCreatedAtDesc(Long familyId);
 
