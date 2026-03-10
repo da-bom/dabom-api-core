92e510e5265978185adc6f17f5c90114d4618710
paul0755 <100present@naver.com>
2026-03-11 02:17:31 +0900

    DABOM-286 style : 코드 스타일 체크

--- src/main/java/com/project/global/exception/code/AppealErrorCode.java
+++ src/main/java/com/project/global/exception/code/AppealErrorCode.java
@@ -22,8 +22,7 @@ public enum AppealErrorCode implements BaseErrorCode {
     APPEAL_NOT_CANCELLABLE(HttpStatus.CONFLICT, "APPEAL_009", "취소할 수 없는 상태입니다."),
     APPEAL_EMERGENCY_CANCEL_NOT_ALLOWED(
             HttpStatus.BAD_REQUEST, "APPEAL_010", "EMERGENCY 타입은 취소할 수 없습니다."),
-    APPEAL_ALREADY_PENDING(
-            HttpStatus.CONFLICT, "APPEAL_011", "같은 정책에 대해 진행 중인 이의제기가 이미 존재합니다.");
+    APPEAL_ALREADY_PENDING(HttpStatus.CONFLICT, "APPEAL_011", "같은 정책에 대해 진행 중인 이의제기가 이미 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String customCode;
