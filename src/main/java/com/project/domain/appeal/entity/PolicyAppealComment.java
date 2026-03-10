f51373ff442ea00db9d991873c058fb5fb7cdf51
paul0755 <100present@naver.com>
2026-03-11 01:19:29 +0900

    DABOM-286 feat : 어필 댓글 entity, Repository 생성

---
+++ src/main/java/com/project/domain/appeal/entity/PolicyAppealComment.java
@@ -0,0 +1,49 @@
+package com.project.domain.appeal.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import com.project.global.util.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/** 이의제기 댓글 엔티티 */
+@Entity
+@Table(name = "policy_appeal_comment")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PolicyAppealComment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "appeal_id", nullable = false)
+    private Long appealId;
+
+    @Column(name = "author_id", nullable = false)
+    private Long authorId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String comment;
+
+    public void delete() {
+        softDelete();
+    }
+
+    public LocalDateTime getDeletedAtValue() {
+        return getDeletedAt();
+    }
+}
