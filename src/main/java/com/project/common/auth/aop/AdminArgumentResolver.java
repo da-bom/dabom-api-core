5ef95dbb15c7b63b57f990f418597cf25c413940
swthewhite <swthewhite@gmail.com>
2026-03-17 11:51:44 +0900

    DABOM-479 feat: AdminId 어노테이션 및 ArgumentResolver 추가
    - @AdminId 파라미터 어노테이션 생성
    - AdminArgumentResolver 생성 (JWT에서 adminId 추출 + ADMIN role 검증)
    - WebConfig에 AdminArgumentResolver 등록

---
+++ src/main/java/com/project/common/auth/aop/AdminArgumentResolver.java
@@ -0,0 +1,53 @@
+package com.project.common.auth.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.project.common.auth.AuthorizationExtractor;
+import com.project.common.auth.JwtTokenUtil;
+import com.project.common.exception.ApplicationException;
+import com.project.common.exception.code.AdminErrorCode;
+import com.project.domain.customer.enums.RoleType;
+
+import io.jsonwebtoken.Claims;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AdminArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasAnnotation = parameter.hasParameterAnnotation(AdminId.class);
+        boolean hasType = Long.class.isAssignableFrom(parameter.getParameterType());
+
+        return hasAnnotation && hasType;
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory)
+            throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        String token = AuthorizationExtractor.extract(request);
+        Claims claims = jwtTokenUtil.getVerifiedClaims(token);
+
+        String role = claims.get("role", String.class);
+        if (!RoleType.ADMIN.name().equals(role)) {
+            throw new ApplicationException(AdminErrorCode.ADMIN_FORBIDDEN);
+        }
+
+        return Long.parseLong(claims.getSubject());
+    }
+}
